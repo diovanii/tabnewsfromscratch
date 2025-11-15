1991951259f0c94fd4997203e1f579d0073aa86b
Diovani Dal'Moro <47093916+diovanii@users.noreply.github.com>
2025-11-15 00:07:19 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Implementação do tabnews pro curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Rosinha, meu dengo de pantufa</h1>
+  return <h1>Rosinha, meu dengo de pantufa</h1>;
 }
 
-export default Home
+export default Home;
