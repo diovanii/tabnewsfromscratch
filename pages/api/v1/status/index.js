cc4bbdde1e75f8cc00db6cb7d402004c0475337d
Diovani Dal'Moro <47093916+diovanii@users.noreply.github.com>
2025-11-17 16:02:47 +0000

    Adiciona os arquivos 'database.js' e '.env'

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as soma_de_1_mais_1;");
+
+  console.log(result.rows);
+
   response.status(200).json({
     chave: "são açima da média <3",
   });
