fef63ef5302aafe4cfe09e3c35c98ef27f8fe631
Diovani Dal'Moro <47093916+diovanii@users.noreply.github.com>
2026-02-04 21:07:38 +0000

    feat: add 'injectAnonymousOrUser' and 'canRequest' middlewares to '/sessions'

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -82,7 +82,25 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("🟢 Login", async () => {});
+  test("🟢 Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "usuarioFluxoRegistro@samba.com",
+          password: "senha Mirabolante",
+        }),
+      },
+    );
+
+    expect(createSessionResponse.status).toBe(201);
+    const createSessionResponseBody = await createSessionResponse.json();
+    expect(createSessionResponseBody.user_id).toBe(createdUserResponseBody.id);
+  });
 
   test("🟢 Get user information", async () => {});
 });
